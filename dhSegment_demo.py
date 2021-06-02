b46148e65945f68ad063cfd7ec0a3a9767602fce
Rémi Petitpierre <38254995+RPetitpierre@users.noreply.github.com>
2021-06-02 22:51:51 +0200

    Add files via upload

--- dhSegment_demo.py
+++ dhSegment_demo.py
@@ -22,13 +22,15 @@
 # 
 # The first 3 cells install dhSegment on the colab notebook, make the necessary imports and load the tensorboard extension to see the training process.
 
-# In[ ]:
+# In[1]:
 
 
-get_ipython().system('pip install git+https://github.com/dhlab-epfl/dhSegment-torch.git@master')
+get_ipython().system('pip install albumentations==0.5.2')
+get_ipython().system('pip install git+https://github.com/RPetitpierre/dhSegment-torch.git@master')
+get_ipython().system('pip install git+https://github.com/martinblech/xmltodict@master')
 
 
-# In[ ]:
+# In[2]:
 
 
 from dh_segment_torch.config import Params
@@ -41,15 +43,18 @@ from dh_segment_torch.training import Trainer
 from dh_segment_torch.inference import PredictProcess
 from dh_segment_torch.post_processing import PostProcessingPipeline
 
-import os, glob, json, cv2
+import os, glob, json, cv2, collections
 import torch
 
 import pandas as pd
 import numpy as np
-import PIL.Image as Image
+from PIL import Image, ImageDraw, ImageColor
+
+import cv2, glob, os, xmltodict, collections
+import xml.etree.ElementTree as ET
 
 
-# In[ ]:
+# In[13]:
 
 
 # Load the TensorBoard notebook extension
@@ -60,7 +65,7 @@ get_ipython().run_line_magic('load_ext', 'tensorboard')
 
 # This cell allows to link the Google colab notebook to your Google drive. It will ask you to provide a specific code to authorize the access.
 
-# In[ ]:
+# In[4]:
 
 
 from google.colab import drive
@@ -71,12 +76,116 @@ drive.mount('/content/drive')
 # 
 # If the cell gives an error, check that you have a 'GPU' runtime in the menu : Runtime > Change Runtime Type > Hardware Accelerator. Then rerun all the above cells again.
 
-# In[ ]:
+# In[5]:
 
 
 assert torch.cuda.device_count() >= 1
 
 print("The GPU has %.2f GB of memory."%(torch.cuda.get_device_properties(0).total_memory//1024**2/1000))
+
+
+# ## CVAT patch
+# 
+# A patch function to convert CVAT to proper annotation format
+
+# In[6]:
+
+
+def drawCVATdataset(data_path, lines_thickness: int = 2, contours_thickness: int = 2, 
+                    contours_color: str = '#FFFFFF'):
+    
+    def getObjectSize(object_, object_type):
+
+        if object_type == 'polygon':
+            points = np.array([[float(pt) for pt in point.split(',')] for point in object_[
+                '@points'].split(';')])
+            extent = np.max(points, axis=0)-np.min(points, axis=0)
+
+            return extent[0]*extent[1]
+
+        elif object_type == 'box':
+            return (float(object_['@xbr'])-float(object_['@xtl']))*(float(object_['@ybr'])-float(object_['@ytl']))
+        
+    def drawObject(object_, object_type='polygon'):
+
+        if object_type == 'polygon':
+            points = np.ravel(np.array([[float(pt) for pt in point.split(',')] for point in object_[
+                '@points'].split(';')])).tolist()
+            draw.polygon(points, fill=classes[object_['@label']])
+            if contours_thickness > 0:
+                draw.line(points + points[:2], fill=contours_color, width=contours_thickness)
+        elif object_type == 'polyline':
+            points = np.ravel(np.array([[float(pt) for pt in point.split(',')] for point in object_[
+                '@points'].split(';')])).tolist()
+            draw.line(points, fill=classes[object_['@label']], width=contours_thickness)
+        elif object_type == 'box':
+            points = [float(object_['@xtl']), float(object_['@ytl']), float(object_['@xbr']), float(object_['@ybr'])]
+            draw.rectangle(points, fill=classes[object_['@label']], outline=contours_color, width=contours_thickness)
+        elif object_type == 'points':
+            points = [[float(pt) for pt in point.split(',')] for point in object_[
+                '@points'].split(';')]
+            for pt in points:
+                point = np.ravel(np.array([(np.array(pt)-(lines_thickness/2)), 
+                                           (np.array(pt)+(lines_thickness/2))])).tolist()
+                draw.ellipse(point, fill=classes[object_['@label']], width=0)
+
+    xml_path = glob.glob('{}/annotations.xml'.format(data_path))
+
+    if len(xml_path) == 0:
+        raise Exception("No annotations file found")
+    elif len(xml_path) > 1:
+        raise Exception("More than one annotation file found")
+
+    tree = ET.parse(xml_path[0])
+    root = tree.getroot()
+    dico = xmltodict.parse(ET.tostring(root))
+
+    labels_path = os.path.join(xml_path[0].replace('annotations.xml', 'labels'))
+    os.makedirs(labels_path, exist_ok=True)
+
+    classes = {'background': '#000000'}
+    for class_ in dico['annotations']['meta']['task']['labels']['label']:
+        classes[class_['name']] = class_['color']
+    if contours_thickness > 0:
+      classes['contours_color'] = contours_color
+
+    labels_list, images_list = [], []
+    for image in dico['annotations']['image']:
+        if ('box' in image.keys()) or ('polygon' in image.keys()) or (
+            'polyline' in image.keys())  or ('points' in image.keys()):
+            canvas = Image.fromarray(np.zeros((int(image['@height']), int(image['@width']), 3), dtype='uint8'))
+            draw = ImageDraw.Draw(canvas)
+
+            areal_objects = []
+            for object_type in ['polygon', 'box']:
+                if (object_type in image.keys()):
+                    if type(image[object_type]) == collections.OrderedDict:
+                        bbox_area = getObjectSize(image[object_type], object_type)
+                        areal_objects.append({'object': image[object_type], 'type': object_type, 'area': bbox_area})
+                    else:
+                        for object_ in image[object_type]:
+                            bbox_area = getObjectSize(object_, object_type)
+                            areal_objects.append({'object': object_, 'type': object_type, 'area': bbox_area})
+
+            if len(areal_objects) > 0:
+                df_areal_objects = pd.DataFrame(areal_objects).sort_values(by='area', ascending=False)
+                for i, row in df_areal_objects.iterrows():
+                    drawObject(row['object'], row['type'])
+            if lines_thickness > 0:
+                for object_type in ['polyline', 'points']:
+                    if (object_type in image.keys()):
+                        if type(image[object_type]) == collections.OrderedDict:
+                            drawObject(image[object_type], object_type)
+                        else:
+                            for object_ in image[object_type]:
+                                drawObject(object_, object_type)
+
+            label_path = os.path.join(labels_path, f"{image['@name'][:-4]}.png")
+            labels_list.append(label_path)
+            images_list.append(os.path.join(labels_path.replace('/labels', '/images'), image['@name']))
+            canvas.save(label_path, format='PNG')
+      
+    return classes, sorted(images_list), sorted(labels_list)
 
 
 # ## Preparing the data
@@ -92,11 +201,11 @@ print("The GPU has %.2f GB of memory."%(torch.cuda.get_device_properties(0).tota
 # 
 # To see if the file are there, you can click on the folder icon in the left panel and mount your own GDrive account.
 
-# In[ ]:
+# In[14]:
 
 
 # Change this parameter to your folder
-folder_name = 'dhSegment-torch-master'
+folder_name = 'dhSegment'
 
 params = {
     'data_path' : '/content/drive/My Drive/{}/data'.format(folder_name), # Path to write the data
@@ -117,12 +226,19 @@ color_label = {
     }
 }
 
+CVAT_convert = {
+    'convert_from_CVAT': False,
+    'lines_thickness': 0,
+    'contours_thickness': 0,
+    'contours_color': '#FFFFFF'
+}
+
 
 # ### Process parameters
 # 
 # The following cell prepares the data according to the parameters defined above.
 
-# In[ ]:
+# In[15]:
 
 
 num_processes = params.pop("num_processes", 4)
@@ -145,8 +261,17 @@ test_csv_path = params.pop("test_csv", os.path.join(data_path, "test.csv"))
 
 params.setdefault("type", "image")
 
-images_list = sorted(glob.glob(os.path.join(images_dir, '*.*')))
-labels_list = sorted(glob.glob(os.path.join(labels_dir, '*.*')))
+if CVAT_convert['convert_from_CVAT']:
+    classes, images_list, labels_list = drawCVATdataset(data_path, 
+          CVAT_convert['lines_thickness'], CVAT_convert['contours_thickness'], 
+          CVAT_convert['contours_color'])
+    labels_dir = labels_list[0][:-1-len(labels_list[0].split('/')[-1])]
+    images_dir = images_list[0][:-1-len(images_list[0].split('/')[-1])]
+    color_label['colors'] = classes
+
+else:
+    labels_list = sorted(glob.glob(os.path.join(labels_dir, '*.*')))
+    images_list = sorted(glob.glob(os.path.join(images_dir, '*.*')))
 
 data = pd.DataFrame({'image': images_list, 'label': labels_list})
 data.to_csv(params['csv_path'], header=False, index=False)
@@ -159,10 +284,15 @@ if data_splitter_params:
     data_splitter = DataSplitter.from_params(data_splitter_params)
     data_splitter.split_data(data, train_csv_path, val_csv_path, test_csv_path)
 
+for class_name in color_label['colors'].keys():
+    if type(color_label['colors'][class_name]) == str:
+        color_label['colors'][class_name] = list(ImageColor.getcolor(
+            color_label['colors'][class_name], "RGB"))
+
 with open(color_label['path'], 'w') as outfile:
     json.dump({'colors': list(color_label['colors'].values()),
-               'one_hot_encoding': None,
-               'labels': list(color_label['colors'].keys())}, outfile)
+              'one_hot_encoding': None,
+              'labels': list(color_label['colors'].keys())}, outfile)
 
 
 # ## Training the model
@@ -171,11 +301,11 @@ with open(color_label['path'], 'w') as outfile:
 # 
 # The defaults should be fine for most use cases.
 # 
-# The only parameter that may require tweaking is the batch size that needs to be set according to the amount of memory the GPU has. Normally, a batch size of 4 is fine for images of c.a. 1M pixels (1000x1000), otherwise a batch size of 2 or less should be set.
+# The only parameter that may require tweaking is the batch size that needs to be set according to the amount of memory the GPU has. For 15-16 GB of memory (what you normally get on Colab), a batch size of 4 is fine for images of c.a. 1M pixels (1000x1000), otherwise a batch size of 2 or less should be set.
 
 # ### Parameters
 
-# In[ ]:
+# In[17]:
 
 
 params = {
@@ -185,7 +315,7 @@ params = {
             "csv_filename":  os.path.join(data_path, "train.csv"),
             "base_dir": data_path,
             "repeat_dataset": 1,
-            "compose": {"transforms": []}#[{"type": "fixed_size_resize", "output_size": 1e6}]} # Resize to a fixed size, could add other transformations.
+            "compose": {"transforms": []}#{"type": "fixed_size_resize", "output_size": 1e6}]} # Resize to a fixed size, could add other transformations.
         },
         "val_dataset": {
             "type": "image_csv", # Validation dataset
@@ -204,16 +334,16 @@ params = {
         "optimizer": {"lr": 5e-5}, # Learning rate
         "lr_scheduler": {"type": "exponential", "gamma": 0.9995}, # Exponential decreasing learning rate
         "val_metric": "+miou", # Metric to observe to consider a model better than another, the + indicates that we want to maximize
-        "early_stopping": {"patience": 10}, # Number of validation steps without increase to tolerate, stops if reached
+        "early_stopping": {"patience": 20}, # Number of validation steps without increase to tolerate, stops if reached
         "model_out_dir": "/content/drive/My Drive/{}/models".format(folder_name), # Path to model output
-        "num_epochs": 100, # Number of epochs for training
+        "num_epochs": 120, # Number of epochs for training
         "evaluate_every_epoch": 2, # Number of epochs between each validation of the model
         "batch_size": 4, # Batch size (to be changed if the allocated GPU has little memory)
         "num_data_workers": 0,
         "track_train_metrics": False,
         "loggers": [
            {"type": 'tensorboard', "log_dir": "/content/drive/My Drive/{}/tensorboard/log".format(folder_name), 
-            "log_every": 5, "log_images_every": 10}, # Tensorboard logging
+            "log_every": 4, "log_images_every": 20}, # Tensorboard logging
            ]
     }
 
@@ -238,7 +368,7 @@ trainer.train()
 # In[ ]:
 
 
-tensorboard --logdir "/content/drive/My Drive/dhSegment-torch-master/tensorboard/log"
+tensorboard --logdir "/content/drive/My Drive/{}/tensorboard/log".format(folder_name)
 
 
 # ## Inference
@@ -281,6 +411,7 @@ process_params = Params({
 
 
 output_dir = "/content/drive/My Drive/{}/predictions".format(folder_name)
+os.makedirs(output_dir, exist_ok=True)
 predict_annots = PredictProcess.from_params(process_params)
 predict_annots.process_to_probas_files(output_dir)
 results = [np.load(path) for path in sorted(glob.glob(os.path.join(output_dir, '*.npy')))]
